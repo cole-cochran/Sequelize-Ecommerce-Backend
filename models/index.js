15c414311d8a00b03abdd45f93b31ae73878288e
Cole Cochran <colecochran405@gmail.com>
2021-10-29 18:18:11 -0500

    creating product tag columns

--- models/index.js
+++ models/index.js
@@ -5,9 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: Tag,
+    unique: false
+  },
+  as: 'product_tag'
+});  
 // Categories have many Products
-
+Category.hasMany(Product, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: Trip,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'location_travellers'
+});
 // Products belongToMany Tags (through ProductTag)
 
 // Tags belongToMany Products (through ProductTag)
