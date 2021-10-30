018c969055a02fc252bdd02add4438c80a02af4d
Cole Cochran <colecochran405@gmail.com>
2021-10-29 19:51:47 -0500

    creating the update category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -21,7 +21,7 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   try {
-    const categoryData = await categoryData.findByPk(req.params.id, {
+    const categoryData = await Category.findByPk(req.params.id, {
       // JOIN with travellers, using the Trip through table
       include: [{ model: Product, }]
     });
@@ -40,7 +40,7 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
   // create a new category
   try {
-    const categoryData = await Location.create(req.body);
+    const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
@@ -49,10 +49,27 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try {
+    const updateCategory = await Category.update({
+      id: req.params.id,
+      category_name: req.body.category_name
+    },{
+      where: {
+        id: req.params.id,
+      }
+    });
+    if(!updateCategory[0]){
+      res.status(404).json({message:"The category with that id cannot be found!"});
+    }
+    res.status(200).json(updateCategory);
+  } catch (error){
+    res.status(500).json(error);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  
 });
 
 module.exports = router;
