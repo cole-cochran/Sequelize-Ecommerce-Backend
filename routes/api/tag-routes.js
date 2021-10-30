9b2be8747b80ecda139ab51edf3a68150c392f13
Cole Cochran <colecochran405@gmail.com>
2021-10-30 12:07:10 -0500

    creating update tag by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -43,6 +43,25 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagPutData = await Tag.update(
+      {
+        tag_name: req.body.tag_name,
+      }, 
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    if (!tagPutData[0]) {
+      res.status(404).json({ message: 'No tag with this id!' });
+      return;
+    }
+    res.status(200).json(tagPutData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
