c0e7d700d3cf54d1e5642be4f2d3e2286eb36fb5
Cole Cochran <colecochran405@gmail.com>
2021-10-30 12:08:10 -0500

    creating delete by tag id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -65,7 +65,22 @@ router.put('/:id', async (req, res) => {
 });
 
 router.delete('/:id', async (req, res) => {
-  // delete on tag by its `id` value
+  try {
+    const tagDeleteData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!tagDeleteData) {
+      res.status(404).json({ message: 'No reader found with that id!' });
+      return;
+    }
+
+    res.status(200).json(tagDeleteData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
